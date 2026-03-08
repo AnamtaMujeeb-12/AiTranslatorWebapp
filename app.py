e0920f571b67aca567b674d16f01639adaa87ef4
Anamta Mujeeb <anamtamujeeb17@gmail.com>
2026-03-08 13:52:58 +0530

    Added UI changes (HTML CSS JS)

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from transformers import MarianMTModel, MarianTokenizer
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+model_name = "Helsinki-NLP/opus-mt-en-hi"
+model = MarianMTModel.from_pretrained(model_name)
+tokenizer = MarianTokenizer.from_pretrained(model_name)
 
-if __name__ == '__main__':
-    app.run(debug = True)
+@app.route("/", methods=["GET", "POST"])
+def translate():
+    translated_text = ""
+
+    if request.method == "POST":
+        text = request.form["data"]
+
+        input_tokens = tokenizer(text, return_tensors="pt", padding=True)
+        output_tokens = model.generate(**input_tokens)
+        translated_text = tokenizer.decode(output_tokens[0], skip_special_tokens=True)
+
+    return render_template("index.html", translated_text=translated_text)
+
+if __name__ == "__main__":
+    app.run(debug=True)
